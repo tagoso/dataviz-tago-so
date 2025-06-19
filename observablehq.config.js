68f61f91d9ad098f14ba2e9325c63e49e920cc8a
tags <shirouo4279@gmail.com>
2025-06-20 00:20:19 +0300

    removed .html

--- observablehq.config.js
+++ observablehq.config.js
@@ -7,8 +7,8 @@ export default {
     {
       name: "Examples",
       pages: [
-        { name: "Dashboard", path: "/example-dashboard.html" },
-        { name: "Report", path: "/example-report.html" }
+        { name: "Dashboard", path: "/example-dashboard" },
+        { name: "Report", path: "/example-report" }
       ]
     }
   ],
